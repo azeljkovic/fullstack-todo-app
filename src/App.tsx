3f7de6f5a9a530ea285cade4b4e02e51c4840d1c
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-08-29 15:43:35 +0200

    created TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { TodoListItem } from './ListItem';
+import React, { useState } from 'react';
+import { TodoList } from './List';
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: 'task 1', complete: true },
   { text: 'task 2', complete: false },
+  { text: 'task 3', complete: false },
 ];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <h1>TODO app</h1>
       <React.Fragment>
-        <TodoListItem todo={todos[0]} />
-        <TodoListItem todo={todos[1]} />
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
       </React.Fragment>
     </div>
   );
